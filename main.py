f5a891eae4cbb89ee5b7a435ccee6de5686e196b
ANIMIXOM <maximshlyamnev@yandex.ru>
2023-12-16 16:49:29 +0300

    \zrwesf

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import Qt
+import random
+from PyQt5 import uic
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)  # Загружаем дизайн
+        self.button_play.clicked.connect(self.create_yellow_circle)
+
+        self.radius = 0
+
+    def create_yellow_circle(self):
+        self.radius = random.randint(50, 200)
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(255, 255, 0))
+        painter.drawEllipse(self.width() / 2 - self.radius / 2, self.height() / 2 - self.radius / 2,
+                            self.radius, self.radius)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setGeometry(100, 100, 400, 300)
+    window.show()
+    sys.exit(app.exec_())
